302ea15febef55720d2c237e43138729fab15d9d
naomi <sarkernr@mail.uc.edu>
2024-09-26 17:50:06 -0400

    lab 5 Task 2 Complete!

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import javax.swing.*;
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner console = new Scanner(System.in);
+        int birthMonth = 0;
+        String trash = "";
+
+        System.out.println("Enter your birth month [1-12]: ");
+        if(console.hasNextInt())
+        {
+            birthMonth = console.nextInt();
+            console.nextLine(); // Clear the buffer
+            if(birthMonth >= 1 && birthMonth <= 12)
+            {
+                System.out.println("You said your birth month is " + birthMonth);
+            }
+            else
+            {
+                System.out.println("Not sure what month " + birthMonth + " is. Run program again!");
+            }
+        }
+        else
+        {
+            trash = console.nextLine();
+            System.out.println("You said your birth month was " + trash);
+            System.out.println("Run the program again!");
+        }
+    }
+}
